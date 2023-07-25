1f8355c4048c9bc68a47746745449517009f7edb
Antonio <antonioribeiroamg@gmail.com>
2023-07-25 07:47:35 -0400

    feat: Created MiddlewareTutorGetValidate Route GET tutors

---
+++ src/infra/routes/tutor.router.ts
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
+import { Router } from 'express';
+import TutorController from '../../app/controllers/TutorController';
+import GetValidation from '../../app/middlewares/validations/TutorGetValidate';
+
+const router = Router();
+
+router.get('/tutors', GetValidation, TutorController.get);
+
+export default router;
