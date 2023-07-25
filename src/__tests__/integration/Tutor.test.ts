d7f5b0b6307489c7579e066821a4a17333e1338e
Antonio <antonioribeiroamg@gmail.com>
2023-07-25 09:14:51 -0400

    test: Created IntegrationTest GET route

--- src/__tests__/integration/Tutor.test.ts
+++ src/__tests__/integration/Tutor.test.ts
@@ -6,6 +6,20 @@ const app = new App();
 
 describe('Integration. Tutor Routes', () => {
   describe('Tutor GET route', () => {
+    it('should return statusCode 400 && ValidateError response with query incorrect', async () => {
+      const sut = { page: 'ValidateError', limit: 'ValidateError' };
+
+      const { body, statusCode } = await request(app.init())
+        .get('/tutors')
+        .query(sut);
+
+      console.log(body);
+      expect(statusCode).toBe(400);
+      expect(body).toEqual({
+        message: 'ValidationError',
+        details: ['page must be a number', 'limit must be a number'],
+      });
+    });
     it('should return statusCode 200 && all tutors response with request correct', async () => {
       const sut = { page: 2, limit: 10 };
 
