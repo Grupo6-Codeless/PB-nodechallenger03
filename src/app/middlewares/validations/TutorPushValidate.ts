ac5d1b1e5ae8201926e1b49f45e5395a99cca9e5
Guilherme Bezerra <88347940+Guilgb@users.noreply.github.com>
2023-07-25 16:59:03 -0300

    Validation errors

---
+++ src/app/middlewares/validations/TutorPushValidate.ts
@@ -0,0 +1,37 @@
+import type { Request, Response, NextFunction } from 'express';
+import Joi from 'joi';
+import { StatusCodes } from 'http-status-codes';
+
+export default (req: Request, res: Response, next: NextFunction): void => {
+  try {
+    const schema = Joi.object({
+      name: Joi.string().min(3).max(30).required(),
+      phone: Joi.string().min(7).required(),
+      email: Joi.string()
+        .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
+        .exist()
+        .required(),
+      password: Joi.string().exist().required(),
+      date_of_birth: Joi.date().required(),
+      zip_code: Joi.string().min(7).required(),
+      pets: Joi.array().required(),
+    });
+
+    const { error } = schema.validate(req.body, {
+      abortEarly: false,
+    });
+
+    if (!(error === undefined)) throw error;
+
+    next();
+  } catch (errors) {
+    const errorsDetails: string[] = [];
+    for (const error of errors.details) {
+      errorsDetails.push(error.message.replace(/\\/g, '').replace(/"/g, ''));
+    }
+    res.status(StatusCodes.BAD_REQUEST).json({
+      message: errors.name,
+      details: errorsDetails,
+    });
+  }
+};
