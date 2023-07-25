a24ad825feef0de03337a3515185fd506557b34d
Guilherme Bezerra <88347940+Guilgb@users.noreply.github.com>
2023-07-25 16:38:29 -0300

    Create push in controller layer

--- src/app/controllers/TutorController.ts
+++ src/app/controllers/TutorController.ts
@@ -1,12 +1,13 @@
 import type { Request, Response } from 'express';
 import TutorService from '../services/TutorService';
+import { StatusCodes } from 'http-status-codes';
 
 class TutorController {
   async get(req: Request, res: Response): Promise<Response> {
     try {
       const result = await TutorService.get(req.query);
 
-      return res.status(200).json(result);
+      return res.status(StatusCodes.OK).json(result);
     } catch (error) {
       if (!(error.statusCode === undefined)) {
         return res.status(error.statusCode).json({
@@ -14,7 +15,22 @@ class TutorController {
           details: error.message,
         });
       }
-      return res.status(500).json(error);
+      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(error);
+    }
+  }
+
+  async push(req: Request, res: Response): Promise<Response> {
+    try {
+      const result = await TutorService.push(req.body);
+      return res.status(StatusCodes.OK).json(result);
+    } catch (error) {
+      if (!(error.statusCode === undefined)) {
+        return res.status(error.statusCode).json({
+          message: error.name,
+          details: error.message,
+        });
+      }
+      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(error);
     }
   }
 }
