9a557daf6b0c92919404cfd909d7fef364bfc1d1
Antonio <antonioribeiroamg@gmail.com>
2023-07-24 20:40:39 -0400

    feat: Created Controller Route GET tutors

--- src/app/controllers/TutorController.ts
+++ src/app/controllers/TutorController.ts
@@ -4,8 +4,16 @@ import TutorService from '../services/TutorService';
 class TutorController {
   async get(req: Request, res: Response): Promise<Response> {
     try {
-      return res.status(201).json();
+      const result = await TutorService.get(req.query);
+
+      return res.status(200).json(result);
     } catch (error) {
+      if (!(error.statusCode === undefined)) {
+        return res.status(error.statusCode).json({
+          message: error.name,
+          details: error.message,
+        });
+      }
       return res.status(500).json(error);
     }
   }
