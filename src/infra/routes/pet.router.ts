2b54718de19a22d272da512478f0f9de89160e39
Antonio <antonioribeiroamg@gmail.com>
2023-07-25 19:55:40 -0400

    feat: Created PetMiddlewareValidate put route

--- src/infra/routes/pet.router.ts
+++ src/infra/routes/pet.router.ts
@@ -1,10 +1,14 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import { Router } from 'express';
 import PetController from '../../app/controllers/PetController';
-// import petCreateAndPutValidation from '../../app/middlewares/validations/PetCreateAndPutValidate';
+import petCreateAndPutValidation from '../../app/middlewares/validations/PetCreateAndPutValidate';
 
 const router = Router();
 
-router.put('/pet/:petId/tutor/:tutorId', PetController.update);
+router.put(
+  '/pet/:petId/tutor/:tutorId',
+  petCreateAndPutValidation,
+  PetController.update
+);
 
 export default router;
