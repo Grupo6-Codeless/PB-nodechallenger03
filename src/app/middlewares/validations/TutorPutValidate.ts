e4ab1828d9a7db40aba22c54592ef3818d12cc2c
Franc1scaGeovanna <fgeovanna111@gmail.com>
2023-07-26 20:23:05 -0300

    feat: Created TutorPutValidate.ts Route PUT tutors

---
+++ src/app/middlewares/validations/TutorPutValidate.ts
@@ -0,0 +1,31 @@
+import type { Request, Response, NextFunction } from 'express';
+import Joi from 'joi';
+
+export default (req: Request, res: Response, next: NextFunction): void => {
+    try {
+        const Tutorschema = Joi.object ({
+            name: Joi.string().required().trim(),
+            phone: Joi.string().required().trim(),
+            email: Joi.string().required().trim(),
+            password: Joi.string().required(),
+            date_of_birth: Joi.date().required(),
+            zip_code: Joi.string().required().trim(),
+        });
+
+        const { error } = Tutorschema.validate(req.body, {
+            abortEarly: false,
+        });
+
+        if (!(error === undefined)) throw error;
+        next();
+    } catch (errors) {
+        const errorsDetails: string[] = [];
+        for (const error of errors.details) {
+            errorsDetails.push(error.message.replace(/\\/g, '').replace(/"/g, ''));
+        }
+        res.status(400).json({
+            message: errors.name,
+            details: errorsDetails,
+        });
+    }
+}
