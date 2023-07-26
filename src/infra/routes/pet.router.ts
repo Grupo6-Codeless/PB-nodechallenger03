1f11fcdb9f0a460711b4b6b9694a7e807ac3250a
Antonio <antonioribeiroamg@gmail.com>
2023-07-26 10:06:04 -0400

    feat: Created Route PATCH Pet

--- src/infra/routes/pet.router.ts
+++ src/infra/routes/pet.router.ts
@@ -1,13 +1,19 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import { Router } from 'express';
 import PetController from '../../app/controllers/PetController';
-import petCreateAndPutValidation from '../../app/middlewares/validations/PetCreateAndPutValidate';
+import petPutValidation from '../../app/middlewares/validations/PetPutValidate';
+import petPatchValidation from '../../app/middlewares/validations/PetPatchValidate';
 
 const router = Router();
 
 router.put(
   '/pet/:petId/tutor/:tutorId',
-  petCreateAndPutValidation,
+  petPutValidation,
+  PetController.update
+);
+router.patch(
+  '/pet/:petId/tutor/:tutorId',
+  petPatchValidation,
   PetController.update
 );
 
