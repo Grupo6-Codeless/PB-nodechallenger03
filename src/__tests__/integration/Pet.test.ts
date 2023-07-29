7f6a9508dcfcdf0a4d780507d894d440f06b9b09
Guilherme Bezerra <88347940+Guilgb@users.noreply.github.com>
2023-07-29 15:17:17 -0300

    feature: Integrarion Test PET delete error 404

--- src/__tests__/integration/Pet.test.ts
+++ src/__tests__/integration/Pet.test.ts
@@ -27,13 +27,25 @@ describe('Integration. Pet Routes', () => {
 
   describe('Pet DELETE route', () => {
     test('should return status code 204', async () => {
-      const petid = '64c555d6bfb93fb1bdc71fdc';
+      const petid = '64c5576f3be063d32def6ab1';
       const tutorid = '64c025e73d7493678bcccc8a';
       const { body, statusCode } = await request(app)
         .delete(`/pet/${petid}/tutor/${tutorid}`)
         .set('Authorization', `Bearer ${token}`);
       expect(statusCode).toBe(204);
       expect(body).toEqual({});
+    });
+    test('should return status code 404', async () => {
+      const petid = '64c555d6bfb93fb1bdc71fdc';
+      const tutorid = '64c025e73d7493678bcccc8a';
+      const { body, statusCode } = await request(app)
+        .delete(`/pet/${petid}/tutor/${tutorid}`)
+        .set('Authorization', `Bearer ${token}`);
+      expect(statusCode).toBe(404);
+      expect(body).toEqual({
+        details: 'Not found Pet',
+        message: 'Not Found Error',
+      });
     });
   });
 });
