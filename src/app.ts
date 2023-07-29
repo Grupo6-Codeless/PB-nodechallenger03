bbf50f732e62aa0008890f8b2b1fada4d0a22346
Antonio <antonioribeiroamg@gmail.com>
2023-07-29 10:51:36 -0400

    feat: Created route /api-docs of swagger

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
 import express from 'express';
 import cors from 'cors';
-// import swaggerUI from 'swagger-ui-express';
+import swaggerUI from 'swagger-ui-express';
 
 import './infra/databases/MongoDatabase';
 import routes from './infra/routes/index.router';
-// import swaggerDocs from './swagger.json';
+import swaggerDocs from './swagger.json';
 
 class App {
   public server: express.Application;
@@ -31,7 +31,7 @@ class App {
 
   private routes(): void {
     this.server.use(...routes);
-    // this.server.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+    this.server.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
   }
 }
 
